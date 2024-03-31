b6a46654acf7605d91d1800013f823419e182e74
Thefreewildddudegames <161177623+Thefreewildddudegames@users.noreply.github.com>
2024-03-31 17:34:33 -0400

    Create math-bot.py

---
+++ math-bot.py
@@ -0,0 +1,42 @@
+# Input function
+def get_user_input(prompt):
+    return input(prompt).strip()
+
+# Calculate function
+def calculate(expression):
+    try:
+        result = eval(expression)
+        print("Result:", result)
+    except Exception as e:
+        print("Error:", e)
+
+# Main Function
+def main():
+    print("""
+█████████████████████████████████████████████████████████
+██▀▄─██▄─▄▄▀█▄─▄█─▄─▄─█─█─█▄─▀█▀─▄█▄─▄▄─█─▄─▄─█▄─▄█─▄▄▄─█
+██─▀─███─▄─▄██─████─███─▄─██─█▄█─███─▄█▀███─████─██─███▀█
+▀▄▄▀▄▄▀▄▄▀▄▄▀▄▄▄▀▀▄▄▄▀▀▄▀▄▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▄▀▄▄▄▄▄▀
+██████████████████████████████████████████████████████████████████████████
+█─▄─▄─█▄─▄▄─█▄─▄▄▀█▄─▄█▄─▀█▀─▄█▄─██─▄█▄─▄████▀▄─██─▄─▄─█─▄▄─█▄─██─▄█▄─▄▄▀█
+███─████─▄█▀██─▄─▄██─███─█▄█─███─██─███─██▀██─▀─████─███─██─██─██─███─▄─▄█
+▀▀▄▄▄▀▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▀▄▄▄▀▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▀▄▄▀▀▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▀▄▄▀""")
+    try:
+        print("what is your name: ")
+        name = get_user_input("")
+        print("Your name is " + name + ".")
+    except Exception as e:
+        print("Error", e)
+
+    
+    while True:
+        print("Enter an arithmetic expression (or 'quit' to exit):")
+        expression = get_user_input("")
+        if expression.lower() == 'quit':
+            print("Exiting...")
+            break
+        else:
+            calculate(expression)
+
+
+main()
